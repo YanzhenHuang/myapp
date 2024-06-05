fe3b3d99b342c99db905437183d86fd629f3da3d
Yanzhen Huang <huangyanzhen0108@163.com>
2024-06-05 15:39:41 +0800

    :bee: Adjust Post Card hover effect.

--- components/uiComponents/Post.tsx
+++ components/uiComponents/Post.tsx
@@ -11,7 +11,7 @@ export const Post = (props: { postModel: PostModel, haveHoverEffect?: boolean })
     let post = props.postModel;
     let haveHoverEffect = props.haveHoverEffect ?? true;
     return (
-        <div className={`bg-white w-96 h-48 overflow-hidden rounded hover:cursor-pointer ${haveHoverEffect && `hover:scale-105`} transition-all`}>
+        <div className={`bg-white w-96 h-48 overflow-hidden rounded hover:cursor-pointer ${haveHoverEffect && `hover:scale-[1.02]`} transition-all`}>
             <Link href={`/posts/${post.id}`} key={post.id} className={'w-full h-full'}>
                 <div className={'w-full h-full  p-5'}>
                     <h2 className={'font-bold'}>{post.id}</h2>
