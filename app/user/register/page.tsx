7b6321efc81d1cb2bb401b1218a1935ddafa0260
Yanzhen Huang <huangyanzhen0108@163.com>
2024-06-06 14:24:30 +0800

    :bee: Settle user registration. Write interface for User Registration and React state var + settings.

--- app/user/register/page.tsx
+++ app/user/register/page.tsx
@@ -1,40 +1,37 @@
 "use client"
 
 import { Main } from "@/components/Frames";
-import React, { useRef, useState } from "react";
-import { FaUser } from '@react-icons/all-files/fa/FaUser';
-import { IconType } from "@react-icons/all-files";
-import PocketBase from 'pocketbase';
+import React, { useState } from "react";
+import PocketBase, { ClientResponseError } from 'pocketbase';
 import { u_p_server } from "@/utils/u_paths";
-
-type State = {
-    'var': string,
-    'func': Function,
-};
+import { AvatarInput, FormInput } from "@/components/uiComponents/Inputs";
+import { UserRegisterModel } from "@/types";
 
 const pb = new PocketBase(u_p_server.BASE_URL);
 
+const RegisterUser = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, json: UserRegisterModel) => {
+    e.preventDefault();
+    try {
+        const record = await pb.collection('users').create<UserRegisterModel>(json);
+    } catch (e: any) {
+        let dataArr = Object.values(e.data.data) as ErrData[];
+        let dataArrKey = Object.keys(e.data.data);
+        let errMessages = e.message + "\n";
 
-const FormInput = (props: { placeholder: string, type?: string, name: string, state: State }) => {
-    const inputStyles = "w-80 p-3 rounded-md border-2 border-gray focus:border-themeColor focus:outline-none";
-    return (
-        <input
-            placeholder={props.placeholder}
-            type={props.type}
-            name={props.name}
-            className={inputStyles}
-            onChange={(e) => {
-                props.state.func(e.target.value);
-            }}></input>
-    );
+        dataArr.map((data, index) => {
+            let indexNum = (index + 1).toString() + " ";
+            errMessages += indexNum + ". " + dataArrKey[index] + ": " + data.message + "\n";
+        });
+        alert(errMessages);
+    }
+};
+
+interface ErrData {
+    code: string;
+    message: string;
 }
 
 export default function Home() {
-    const avatarDivRef = React.createRef<HTMLDivElement>();
-    const fileInputRef = React.createRef<HTMLInputElement>();
-    const avatarRef = React.createRef<IconType>();
-    const formRef = React.createRef<HTMLFormElement>();
-
     // User Name
     const [m_userName, setUserName] = useState<string>("");
 
@@ -49,64 +46,36 @@ export default function Home() {
 
     // Avatar
     const [m_avatar, setAvatar] = useState<Blob>();
-    const [m_avatarURL, setAvatarURL] = useState<string>();
-    const [m_avatarUsrIconDisplay, setAvatarUsrIconDisplay] = useState<"hidden" | "">("");
-
-    const gatherFormData = (e: React.FormEvent<HTMLFormElement>): any => {
-    };
 
     return (
         <Main>
-            <form ref={formRef} >
+            <form>
                 <div className="flex flex-col bg-white gap-5 p-32 items-center">
                     {/* User Avatar Input*/}
-                    <div
-                        ref={avatarDivRef}
-                        onClick={() => { fileInputRef.current?.click(); }}
-                        style={{
-                            backgroundImage: `url(${m_avatarURL})`,
-                            backgroundSize: 'cover',
-                            backgroundRepeat: 'no-repeat',
-                            backgroundPosition: 'center',
-                        }}
-                        className="bg-themeColorUltraLight h-32 w-32 rounded-full hover:cursor-pointer">
+                    <AvatarInput setAvatar={setAvatar} />
 
-                        <input type="file" ref={fileInputRef} className="hidden"
-                            onChangeCapture={(e: React.FormEvent<HTMLInputElement>) => {
-                                let target = e.target as HTMLInputElement;
-                                if (target.files && target.files.length > 0) {
-                                    let fileObj = target.files[0];
-                                    setAvatar(fileObj);
-                                    setAvatarURL(URL.createObjectURL(fileObj));
-                                    setAvatarUsrIconDisplay("hidden");
-                                }
-                            }}></input>
-                        <FaUser ref={avatarRef} className={`w-full h-full p-10 text-white ${m_avatarUsrIconDisplay}`} />
-                    </div>
-
-                    <FormInput placeholder="User Name" type="text" name="usrName" state={{ 'var': m_userName, 'func': setUserName }}></FormInput>
-                    <FormInput placeholder="Email" type="email" name="email" state={{ 'var': m_email, 'func': setEmail }} ></FormInput>
-                    <FormInput placeholder="Set Password" type="password" name="pwd" state={{ 'var': m_pwd, 'func': setPwd }}></FormInput>
-                    <FormInput placeholder="Confirm Password" type="password" name="pwdConfirm" state={{ 'var': m_confirmPwd, 'func': setConfirmPwd }}></FormInput>
+                    {/* Input Informations */}
+                    <FormInput placeholder="User Name" type="text" name="username" state={{ 'state': m_userName, 'setState': setUserName }}></FormInput>
+                    <FormInput placeholder="Email" type="email" name="email" state={{ 'state': m_email, 'setState': setEmail }} ></FormInput>
+                    <FormInput placeholder="Set Password" type="password" name="password" state={{ 'state': m_pwd, 'setState': setPwd }}></FormInput>
+                    <FormInput placeholder="Confirm Password" type="password" name="passwordConfirm" state={{ 'state': m_confirmPwd, 'setState': setConfirmPwd }}></FormInput>
 
                     <button
                         className="text-white bg-themeColor pt-2 pb-2 pl-5 pr-5 rounded-md hover:scale-[1.02] hover:opacity-80 transition-all"
                         onClick={async (e) => {
                             e.preventDefault();
-                            let fd = new FormData();
+
                             let json = {
                                 'username': m_userName,
                                 'email': m_email,
                                 'password': m_pwd,
                                 'passwordConfirm': m_confirmPwd,
                                 'avatar': m_avatar,
-                            };
+                            } as UserRegisterModel;
 
-                            try {
-                                const record = await pb.collection('users').create(json);
-                            } catch (e) {
-                                console.log(e);
-                            }
+                            await RegisterUser(e, json).then(() => {
+                                window.location.href = "/allPosts/1"
+                            });
 
                         }}>Submit</button>
                 </div>
--- types/index.d.ts
+++ types/index.d.ts
@@ -10,3 +10,29 @@ export interface PostModel {
     content: string;
 }
 
+/**
+ * A data model used for user registration.
+ * @member {string} username - The username of the user.
+ * @member {string} email - The email address of the user.
+ * @member {string} password - The password of the user.
+ * @member {string} passwordConfirm - The confirmation of the password.
+ * @member {Blob} avatar - The avatar of the user.
+ */
+export interface UserRegisterModel {
+    username: string;
+    email: string;
+    password: string;
+    passwordConfirm: string;
+    avatar: Blob;
+}
+
+
+/**
+ * A generic state management interface for React components.
+ * @member {T} state - The current state value.
+ * @member {(newState: T) => void} setState - A function to update the state.
+ */
+export interface ReactGetSetState<T> {
+    state: T;
+    setState: (newState: T) => void;
+}
